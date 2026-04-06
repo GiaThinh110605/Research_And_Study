2f6ff952b98a6a7d05d38719065df877908d6494
Khang Nguyen <nghmanhkhang@gmail.com>
2026-04-06 14:13:31 +0700

    Test + TestResult + GPA

--- backend/app/api/v1/api.py
+++ backend/app/api/v1/api.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter
-from app.api.v1.routes import auth, users
+from app.api.v1.routes import auth, flashcards, gpa, tests, users
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(flashcards.router, prefix="/flashcards", tags=["flashcards"])
+api_router.include_router(gpa.router, prefix="/gpa", tags=["gpa"])
+api_router.include_router(tests.router, prefix="/tests", tags=["tests"])
